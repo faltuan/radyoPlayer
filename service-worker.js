d2e41433e56802a3c7cff6cce8939e6783cd1423
faltuan <83354250+faltuan@users.noreply.github.com>
2025-11-15 12:03:02 +0300

    up

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,20 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("fgold-cache").then((cache) => {
-      return cache.addAll(["/radyoPlayer/index.html", "/radyoPlayer/icon-512.png", "/radyoPlayer/manifest.json"]);
-    })
-  );
+    e.waitUntil(
+        caches.open("fgold-cache").then((cache) => {
+            return cache.addAll([
+                "/radyoPlayer/",
+                "/radyoPlayer/index.html",
+                "/radyoPlayer/icon-512.png",
+                "/radyoPlayer/manifest.json"
+            ]);
+        })
+    );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+    e.respondWith(
+        caches.match(e.request).then((response) => {
+            return response || fetch(e.request);
+        })
+    );
 });
